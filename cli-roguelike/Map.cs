8083451baebfe24dcecda909ffac294a585e478c
terabvte <marcofediuc@gmail.com>
2025-06-18 19:10:40 +0200

    Define Map constructor; Begin Generate() method

--- cli-roguelike/Map.cs
+++ cli-roguelike/Map.cs
@@ -2,7 +2,27 @@ namespace cli_roguelike;
 
 public class Map
 {
-    public int Width { get; set; }
-    public int Height { get; set; }
-     
+    public int Width { get; private set; }
+    public int Height { get; private set; }
+
+    private Tile[,] _tiles;
+
+    public Map(int width, int height)
+    {
+        Width = width;
+        Height = height;
+
+        _tiles = new Tile[width, height];
+    }
+
+    public void Generate()
+    {
+        for (int i = 0; i < Width; i++) 
+        { 
+            for (int j = 0; j < Height; j++)
+            {
+                _tiles[i, j].Type = TileType.Wall;
+            } 
+        }  
+    }
 }
